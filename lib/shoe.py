d1d00930d1302b373c2dcdf1852ef3b0e9abb627
Naomi Lagat <naomiejerutoe@gmail.com>
2024-03-01 10:47:28 +0300

    completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        """size must be an integer"""
+        return self._size 
+    
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if(type(size) in (int, float)):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self, condition = "New"):
+        print("Your shoe is as good as new!")
+        self.condition = condition
